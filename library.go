2aa0eec3c46eec5f9b0aac35b8afa2c2f9764341
hGriff0n <ghooper96@gmail.com>
2017-10-08 19:39:27 -0400

    Added Go Implementation
    Adds a very basic implementation of the library storage utility utilizing a basic key-value database

--- library.go
+++ library.go
@@ -1,50 +1,70 @@
-package library
+package main
 
 import (
 	"os"
-	"sort"
+	"encoding/json"
+	"fmt"
+	"strconv"
 
 	"github.com/urfave/cli"	// https://github.com/urfave/cli
-
-	// "github.com/jinzhu/gorm"
-    // _ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/boltdb/bolt" // https://github.com/boltdb/bolt
 )
 
 // A simple command line program to record information about my home library
 
-// Might convert this over to another language
-// The sql driver doesn't work on windows ??
-
 type Book struct {
-	gorm.Model
-	// ISBN `gorm:"primary_key"`
 	Title string
-	Pages uint
-	Read uint
+	Pages uint64
+	Read uint64
 }
 
 func main() {
 	// Setup and connect to the database
-	// db, err := gorm.Open("sqlite3", "C:/Users/ghoop/Desktop/library/library.db")
-	// if err != nil {
-	// 	panic("Failed to connect to database")
-	// }
-	// defer db.Close()
-
-	// db.AutoMigrate(&Book{})
+	db, err := bolt.Open("books.db", 0600, nil)
+	if err != nil {
+		// log.Fatal(err)
+	}
+	db.Update(func(tx *bolt.Tx) error {
+		// Create the initial bucket
+		_, err := tx.CreateBucketIfNotExists([]byte("books"))
+		return err
+	})
+	defer db.Close()
 
 
 	app := cli.NewApp()
 
+	// TODO: Extend this with more capabilities (better command line args)
+	// TODO: Figure out a better way of organizing everything (better schema)
 	app.Commands = []cli.Command{
 		{
 			Name:    "add",
 			Aliases: []string{"a"},
 			Usage:   "add a new book to the list",
 			Action:  func(c *cli.Context) error {
-				// file.Append(c.Args()[0] + "0 0")
-				db.Create(&Book{Title: c.Args()[0], Pages: 0, Read: 0})
-				return nil
+				return db.Update(func(tx *bolt.Tx) error {
+					bucket := tx.Bucket([]byte("books"))
+					pages, err := strconv.ParseUint(c.Args()[2], 0, 64)
+					if err != nil {
+						return err
+					}
+
+					read, err := strconv.ParseUint(c.Args()[1], 0, 64)
+					if err != nil {
+						return err
+					}
+
+					encoded, err := json.Marshal(Book{ 
+						Title: c.Args()[0],
+						Pages: pages,
+						Read: read,
+					})
+					if err != nil {
+						return err
+					}
+
+					return bucket.Put([]byte(c.Args()[0]), encoded)
+				})
 			},
 		},
 		{
@@ -52,10 +72,29 @@ func main() {
 			Aliases: []string{"u"},
 			Usage:   "update the information for a given book",
 			Action:  func(c *cli.Context) error {
-				// var book Book
-				// db.First(&book, "title = ?", "")
-				// TODO:
-				return nil
+				return db.Update(func(tx *bolt.Tx) error {
+					bucket := tx.Bucket([]byte("books"))
+					v := bucket.Get([]byte(c.Args()[0]))
+
+					var book Book
+					if err := json.Unmarshal(v, &book); err != nil {
+						return err
+					}
+					
+					read, err := strconv.ParseUint(c.Args()[1], 0, 64)
+					if err != nil {
+						return err
+					}
+
+					book.Read = read
+
+					encoded, err := json.Marshal(book)
+					if err != nil {
+						return err
+					}
+
+					return bucket.Put([]byte(c.Args()[0]), encoded)
+				})
 			},
 		},
 		{
@@ -63,10 +102,10 @@ func main() {
 			Aliases: []string{"u"},
 			Usage:   "update the information for a given book",
 			Action:  func(c *cli.Context) error {
-				// var book Book
-				// db.First(&book, "title = ?", "")
-				// db.Delete(&book)
-				return nil
+				return db.Update(func(tx *bolt.Tx) error {
+					bucket := tx.Bucket([]byte("books"))
+					return bucket.Delete([]byte(c.Args()[0]))
+				})
 			},
 		},
 		{
@@ -74,12 +113,29 @@ func main() {
 			Aliases: []string{"s"},
 			Usage:   "display the information",
 			Action:  func(c *cli.Context) error {
-				// var books []Book
-				// db.Find(&books)
+				return db.View(func(tx *bolt.Tx) error {
+					bucket := tx.Bucket([]byte("books"))
+					cur := bucket.Cursor()
 
-				return nil
+					var totalPages uint64 = 0
+					var readPages uint64 = 0
+					for k,v := cur.First(); k != nil; k,v = cur.Next() {
+						var book Book
+						if err := json.Unmarshal(v, &book); err != nil {
+							return err
+						}
+
+						fmt.Printf("Title: %v - Total: %v - Read: %v\n", string(k), book.Pages, book.Read)
+						totalPages += book.Pages
+						readPages += book.Read
+					}
+					
+					fmt.Printf("\nRead Pages: %v\nTotal Pages: %v\n", readPages, totalPages)
+
+					return nil
+				})
 			},
-		}
+		},
 	}
 
 	// What is this doing ???
